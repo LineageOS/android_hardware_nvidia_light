ad1f5902d10ffda523cebda2bca3cf19d5d290ff
Aaron Kling <webgeek1234@gmail.com>
2019-05-02 20:50:37 -0500

    Initial stub commit

---
+++ service.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.light@2.0-service-nvidia"
+
+// #define LOG_NDEBUG 0
+
+#include <android/log.h>
+#include <hidl/HidlTransportSupport.h>
+#include <hardware/lights.h>
+
+using android::sp;
+using android::status_t;
+using android::OK;
+
+// libhwbinder:
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+
+// Generated HIDL files
+using android::hardware::light::V2_0::implementation::Light;
+
+int main() {
+
+    ALOGI("Light HAL Service 2.0 for Nvidia is starting.");
+
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    ALOGI("Light Service is ready");
+    joinRpcThreadpool();
+    //Should not pass this line
+
+shutdown:
+    // In normal operation, we don't expect the thread pool to exit
+
+    ALOGE("Light Service is shutting down");
+    return 1;
+}
